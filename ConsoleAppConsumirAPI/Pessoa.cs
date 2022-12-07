8e294e4baf6828c46b4e3c0750a4fbafbec50d98
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-12-07 20:41:35 -0300

    Add ConsoleAppConsumirAPI project files

---
+++ ConsoleAppConsumirAPI/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleAppConsumirAPI
+{
+    internal class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ ConsoleAppConsumirAPI/Program.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace ConsoleAppConsumirAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string BaseUrl = "http://localhost:5291/";
+            int opcao = 0;
+
+            do
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine("1 - Efeturar Consulta");
+                Console.WriteLine("2 - Efeturar Cadastro");
+                Console.WriteLine("0 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                switch (opcao)
+                {
+                    case 1:
+                        List<Pessoa>? pessoas = new List<Pessoa>();
+
+                        HttpClient client = new HttpClient();
+                        client.BaseAddress = new Uri(BaseUrl);
+                        client.DefaultRequestHeaders.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(
+                            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage response = await client.GetAsync("api/pessoas");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var dados = response.Content.ReadAsStringAsync().Result;
+                            pessoas = JsonConvert.DeserializeObject<List<Pessoa>>(dados);
+
+                            foreach (Pessoa item in pessoas)
+                            {
+                                Console.WriteLine(item.Id + " - " + item.Nome);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Erro: " + response.StatusCode.ToString());
+                        }
+                        break;
+                    case 2:
+                        Pessoa p = new Pessoa();
+                        Console.WriteLine("Informe o nome da pessoa:");
+                        p.Nome = Console.ReadLine();
+
+                        try
+                        {
+                            HttpClient clientPost = new HttpClient();
+                            HttpResponseMessage responsePost = await clientPost.PostAsJsonAsync(
+                                BaseUrl + "api/pessoas", p);
+                            Console.WriteLine("Response: " + responsePost.StatusCode);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Erro: " + e.Message);
+                        }
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida, tente novamente!");
+                        break;
+                }
+            } while (opcao != 0);         
+        }
+    }
+}
