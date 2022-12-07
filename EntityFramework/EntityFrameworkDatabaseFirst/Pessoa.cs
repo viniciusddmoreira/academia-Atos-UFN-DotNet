8fc481dafa9cd4dfa99313251b53f8c026280b1d
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-12-07 20:40:30 -0300

    Add EntityFramework projects

---
+++ EntityFramework/EntityFrameworkDatabaseFirst/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDatabaseFirst;
+
+public partial class Pessoa
+{
+    public int Id { get; set; }
+
+    public string nome { get; set; } = null!;
+
+    public virtual ICollection<Email> Emails { get; set; } = new List<Email>();
+}
