4fa7753e1f4609cc9f065e03b280305a8f7e540e
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-12-09 05:30:49 -0300

    Add ProjetoMVC project files

---
+++ ProjetoMVC/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Welcome(string nome, int numTimes = 1)
+        {
+            ViewData["Message"] = "Ola " + nome;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
